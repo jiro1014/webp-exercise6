7dfc11c6fff0f7cfb08b6c66cf922322b013c4a8
jiro1014 <73224696+jiro1014@users.noreply.github.com>
2021-01-04 17:41:33 +0900

    my sixth web site

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ for(let i = 1; i <= 5; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', 'images/pic' + i + '.jpg');
     thumbBar.appendChild(newImage);
-    newImages.onclick = function(e) {
+    newImage.onclick = function(e) {
         displayedImage.src = e.target.src;
     }
 }
@@ -17,14 +17,14 @@ for(let i = 1; i <= 5; i++) {
 
 /* Wiring up the Darken/Lighten button */
 btn.onclick = function() {
-    const btnClass = btn.getAttribute("class");
-    if(btnClass === "dark") {
-        btn.setAttribute("class","light");
-        btn.textContent = "Lighten";
-        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
     }else {
-        btn.setAttribute("class","dark");
-        btn.textContent = "Darken";
-        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
     }
 }
